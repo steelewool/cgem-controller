0f80ae218994a5e7a219a603e3fea73fe282a99b
Zach Steele <zach@steelewoolsoftware.com>
2018-09-09 16:00:39 -0700

    Convert script to object+main

--- convertRaDecToCgemUnits.py
+++ convertRaDecToCgemUnits.py
@@ -1,55 +1,70 @@
 
-raHr   = input ('raHr   : ')
-raMin  = input ('raMin  : ')
-raSec  = input ('raSec  : ')
-
-decDeg = input ('decDeg : ')
-decMin = input ('decMin : ')
-decSec = input ('decSec : ')
-
 # Algorith doesn't handle negavite degrees. Negative angle much be translated
 # to be between 270 and 360 degrees.
 
-softwareResolution = 2**24;
-fullCircleDeg      = 360
-fullCircleSec      = fullCircleDeg * 60.0 * 60.0
-
-oneTwelthArcSeconds = fullCircleSec * 12.0
-conversionFactor    = softwareResolution / oneTwelthArcSeconds
-
-decInSeconds =  abs(decDeg) * 60.0 * 60.0 + decMin * 60.0 + decSec
-
-if (decDeg < 0):
-    decInSeconds = (360.0 * 60.0 * 60.0) - decInSeconds;
+class RaDecToCgem:
+    softwareResolution = 2**24;
+    fullCircleDeg      = 360
+    fullCircleSec      = fullCircleDeg * 60.0 * 60.0
     
-raInSeconds  = (raHr   * 60.0 * 60.0 + raSec  * 60.0 + raSec) * 15.0
+    oneTwelthArcSeconds = fullCircleSec * 12.0
+    conversionFactor    = softwareResolution / oneTwelthArcSeconds
+    
+    def __init__(self, decDeg, decMin, decSec):
+        self.decInSeconds =  abs(decDeg) * 60.0 * 60.0 + decMin * 60.0 + decSec
+        
+        if (decDeg < 0):
+            self.decInSeconds = (360.0 * 60.0 * 60.0) - self.decInSeconds;
+        
+        self.raInSeconds  = (raHr   * 60.0 * 60.0 + raSec  * 60.0 + raSec) * 15.0
+#        self.decGotoValue = self.decInSeconds * 12.0 * RaDecToCgem.conversionFactor
+#        self.raGotoValue  = self.raInSeconds  * 12.0 * RaDecToCgem.conversionFactor
+        self.decGotoValue = convertSeconds(self.decInSeconds)
+        self.raGotoValue  = convertSeconds(self.raInSeconds)
+        
+#        self.decHighByte = int (self.decGotoValue / 256 / 256)
+#        self.decMidByte  = int ((self.decGotoValue - (self.decHighByte * 256 * 256)) / 256)
+#        self.decLowByte  = int (self.decGotoValue - (self.decHighByte * 256 * 256) - (self.decMidByte * 256))
+#        self.raHighByte  = int (self.raGotoValue  / 256 / 256)
+#        self.raMidByte   = int ((self.raGotoValue -  (self.raHighByte  * 256 * 256)) / 256)
+#        self.raLowByte   = int (self.raGotoValue  - (self.raHighByte  * 256 * 256) - (self.raMidByte  * 256))
+        self.decHighByte, self.decMidByte, self.decLowByte = highMidLow(self.decGotoValue)
+        self.raHighByte, self.raMidByte, self.raLowByte = highMidLow(self.raGotoValue)
+    
+    def convertSeconds(seconds):
+        return seconds * 12.0 * RaDecToCgem.conversionFactor
+    
+    def highMidLow(gotoValue):
+        highByte = int (gotoValue  / 256 / 256)
+        midByte  = int ((gotoValue - (highByte  * 256 * 256)) / 256)
+        lowByte  = int (gotoValue  - (highByte  * 256 * 256) - (midByte  * 256))
+        return [highByte, midByte, lowByte]
 
-decGotoValue = decInSeconds * 12.0 * conversionFactor
-raGotoValue  = raInSeconds  * 12.0 * conversionFactor
-
-decHighByte = int (decGotoValue / 256 / 256)
-decMidByte  = int ((decGotoValue - (decHighByte * 256 * 256)) / 256)
-decLowByte  = int (decGotoValue - (decHighByte * 256 * 256) - (decMidByte * 256))
-
-raHighByte  = int (raGotoValue  / 256 / 256)
-raMidByte   = int ((raGotoValue -  (raHighByte  * 256 * 256)) / 256)
-raLowByte   = int (raGotoValue  - (raHighByte  * 256 * 256) - (raMidByte  * 256))
-
-print 'RA   hr min sec      : ', raHr,   ' ', raMin,  ' ', raSec
-print 'Dec deg min sec      : ', decDeg, ' ', decMin, ' ', decSec
-print 'softwareResolution   : ', softwareResolution
-print 'fullCircleSec        : ', fullCircleSec
-print 'oneTwelthArcSeconds  : ', oneTwelthArcSeconds
-print 'conversionFactor     : ', conversionFactor
-print 'decInSeconds         : ', decInSeconds
-print 'raInSeconds          : ', raInSeconds
-print 'hex-int decGotoValue : ', hex(int(decGotoValue))
-print 'hex-int raGotValuie  : ', hex(int(raGotoValue))
-print 'hex decHighByte      : ', hex(decHighByte)
-print 'hex decMidByte       : ', hex(decMidByte)
-print 'hex decLowByte       : ', hex(decLowByte)
-print 'hex raHighByte       : ', hex(raHighByte)
-print 'hex raMidByte        : ', hex(raMidByte)
-print 'hex raLowByte        : ', hex(raLowByte)
-
+if __name__ == '__main__':
+    raHr   = input ('raHr   : ')
+    raMin  = input ('raMin  : ')
+    raSec  = input ('raSec  : ')
+    
+    decDeg = input ('decDeg : ')
+    decMin = input ('decMin : ')
+    decSec = input ('decSec : ')
+    
+    conversion = RaDecToCgem(decDec, decMin, decSec)
+    
+    print 'RA   hr min sec      : ', raHr,   ' ', raMin,  ' ', raSec
+    print 'Dec deg min sec      : ', decDeg, ' ', decMin, ' ', decSec
+    print 'softwareResolution   : ', RaDecToCgem.softwareResolution
+    print 'fullCircleSec        : ', RaDecToCgem.fullCircleSec
+    print 'oneTwelthArcSeconds  : ', RaDecToCgem.oneTwelthArcSeconds
+    print 'conversionFactor     : ', RaDecToCgem.conversionFactor
+    print 'decInSeconds         : ', conversion.decInSeconds
+    print 'raInSeconds          : ', conversion.raInSeconds
+    print 'hex-int decGotoValue : ', hex(int(conversion.decGotoValue))
+    print 'hex-int raGotValuie  : ', hex(int(conversion.raGotoValue))
+    print 'hex decHighByte      : ', hex(conversion.decHighByte)
+    print 'hex decMidByte       : ', hex(conversion.decMidByte)
+    print 'hex decLowByte       : ', hex(conversion.decLowByte)
+    print 'hex raHighByte       : ', hex(conversion.raHighByte)
+    print 'hex raMidByte        : ', hex(conversion.raMidByte)
+    print 'hex raLowByte        : ', hex(conversion.raLowByte)
 
