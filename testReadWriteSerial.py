f80d91a748a106274305108be50154026a89ab50
Rob Steele <robert@steelewoolsoftware.com>
2018-09-11 07:33:24 -0700

    Runs using a loop back on my new serial cable. I had to run with sudo prefixed in front of 'python testReadWriteSerial.py'.

--- testReadWriteSerial.py
+++ testReadWriteSerial.py
@@ -7,20 +7,21 @@
 import serial
 
 ser0 = serial.Serial('/dev/ttyUSB0', timeout=1) # open serial port
-ser1 = serial.Serial('/dev/ttyUSB1', timeout=1) # open 2nd serial port
+# ser1 = serial.Serial('/dev/ttyUSB1', timeout=1) # open 2nd serial port
 
 print 'ser0.name : ', ser0.name    # check which port was really used
-print 'ser1.name : ', ser1.name
+# print 'ser1.name : ', ser1.name
 
 print 'write hello from serial 0'
 
 ser0.write(b'hello')            # write a string
 
-print 'read from serial port 1'
+print 'read from serial port'
 
-char = ser1.read(20)            # read a character
+char = ser0.read(20)            # read a character
+# char = ser1.read(20)            # read a character
 
 print 'char     : ', char       # print the output
 
 ser0.close()                     # close port
-ser1.close()
+# ser1.close()
