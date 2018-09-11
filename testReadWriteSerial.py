2780d9cc7c23e24417805de0a22022fa0db4575b
Rob Steele <robert@steelewoolsoftware.com>
2018-09-10 17:50:31 -0700

    Used two serial ports with a null modelm and gender bender connectors. Test worked on 9 September 2018.

--- testReadWriteSerial.py
+++ testReadWriteSerial.py
@@ -1,12 +1,26 @@
+# This is using two serial to USB cables connected together with a
+# null modem and gender bender connector. It worked on 9/10/2018.
+# This has the disadvantage of using two usb ports and required two
+# cables. But I feel it is more real life than the loop back test
+# done with haywires.
+
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', timeout=2) # open serial port
-print 'ser.name : ', ser.name    # check which port was really used
-print 'write hello'
+ser0 = serial.Serial('/dev/ttyUSB0', timeout=1) # open serial port
+ser1 = serial.Serial('/dev/ttyUSB1', timeout=1) # open 2nd serial port
 
-ser.write(b'hello')             # write a string
+print 'ser0.name : ', ser0.name    # check which port was really used
+print 'ser1.name : ', ser1.name
 
-char = ser.read(20)             # read a character
+print 'write hello from serial 0'
+
+ser0.write(b'hello')            # write a string
+
+print 'read from serial port 1'
+
+char = ser1.read(20)            # read a character
+
 print 'char     : ', char       # print the output
 
-ser.close()                     # close port
+ser0.close()                     # close port
+ser1.close()
