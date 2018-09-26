b17987579e0896dd949e5fd4f3bc1b4e9082f7cd
Rob Steele <robert@steelewoolsoftware.com>
2018-09-26 11:34:03 -0700

    Added in logic for astropy - still a work in progress.

--- objectRaDec.py
+++ objectRaDec.py
@@ -5,7 +5,6 @@
 # As of 9/25/18 I have the LST embedded in the ObjectRaDec object. It may make more
 # sense to place in the list - but but is still a deferred design detail.
 
-
 class Ra:
     def __init__ (self, hr = 0, min = 0, sec = 0):
         self.hr  = hr
@@ -13,6 +12,7 @@ class Ra:
         self.sec = sec
     def getSeconds(self):
         return ((self.hr * 60.0 * 60.0) + (self.min  * 60.0) + self.sec) * 15.0
+    # define subtracttion
     def __sub__ (self, y):
         xSeconds = self.getSeconds()
         ySeconds = y.getSeconds()
@@ -35,7 +35,7 @@ class Lst:
         return ((self.hr * 60.0 * 60.0) + (self.min * 60.0) + self.sec) * 15.0
 
 class ObjectRaDec:
-    def __init__ (self, ra=Ra(), dec=Dec(), lst=Lst()):
+    def __init__ (self, ra = Ra(), dec = Dec(), lst = Lst()):
         self.ra  = ra
         self.dec = dec
         self.lst = lst
@@ -47,6 +47,8 @@ class ObjectRaDec:
 
     def localHrAngle(self):
         return int((self.ra - self.lst)/(3600*15))
+
+    # This function will the bin value for searching and sorting. 
     def bin (self):
         if (self.dec.deg > 70):
             return 1
@@ -59,12 +61,14 @@ class ObjectRaDec:
 
 # First determine which bin the two objects are in which is based on the LST.
 
+    # define == operator
     def __eq__ (x,y):
         if ((x.ra == y.ra) and (x.dec == y.dec)):
             return 1
         else:
             return 0
 
+    # define < operation for 
     def __lt__ (x,y):
         xRaInSeconds  = x.ra.getSeconds()
         yRaInSeconds  = y.ra.getSeconds()
@@ -75,30 +79,58 @@ class ObjectRaDec:
         print yRaInSeconds
         print xDecInSeconds
         print yDecInSeconds
+        return 0
 
 # If the objects declination is > 70 degrees north then it goes in bin 1. The other 12 bins start
 # with the local angle of -90 degree going each by 15 degree increments until I get to +90 degree.
 
-        return 0
-
 # Yet another questions, whey am I having to use getLst() instead of lst?
-
+        
     def write(self):
         print '   RA   hr min sec: ' + str(self.ra.hr)   + ':' + str(self.ra.min)  + ':' + str(self.ra.sec)
         print '   Dec deg min sec: ' + str(self.dec.deg) + ':' + str(self.dec.min) + ':' + str(self.ra.sec)
         print '   LST  hr min sec: ' + str(self.lst.hr)  + ':' + str(self.lst.min) + ':' + str(self.lst.sec)
+        print
 
 # Using the paradigm supplied by Zach to be able to test this class
 
 if __name__ == '__main__':
 
+    import astropy.time
+    from   astropy.time        import Time
+    from   astropy             import units as u
+    from   astropy.coordinates import EarthLocation
+    from   astropy.coordinates import SkyCoord
+
+    observingPosition = EarthLocation(lat    = (34+49/60+32/3600) *u.deg,
+                                      lon    =-(118+1/60+27*3600) *u.deg,
+                                      height = (5000 * 0.3048)    *u.m)
+    
+    print 'observingPosition            : ', observingPosition
+    print 'astropy.time.Time.now()      : ', astropy.time.Time.now()
+
+    date = astropy.time.Time(astropy.time.Time.now(),
+                             scale='utc',
+                             location=observingPosition)
+    
+    print 'date                         : ', date
+    print 'date.now()                   : ', date.now()
+    print 'date.sidereal_time(mean)     : ', date.sidereal_time('mean')
+    print 'date.sidereal_time(apparent) : ', date.sidereal_time('apparent')
+
+    meanLST = date.sidereal_time('mean')
+    print 'meanLST                      : ', meanLST
+    
+    print '----------------------'  
+    
     object1 = ObjectRaDec(Ra(12,13,14),Dec(1,2,3), Lst(4,5,6))
     object2 = ObjectRaDec(Ra(0,1,2),   Dec(3,4,5), Lst(6,7,8))
     object3 = object2
-    
-    print object1.write()
-    print object2.write()
 
+    object1.write()
+    object2.write()
+    object3.write()
+    
     print 'subtract ra  : ', object1.ra - object2.ra
     print 'localHrAngle : ', object1.localHrAngle()
     print 'bin          : ', object1.bin()
