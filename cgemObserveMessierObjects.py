12bc3cc8137a1402066be1c66194a223e7a5a8f5
Rob Steele <robert@steelewoolsoftware.com>
2018-09-30 08:18:00 -0700

    Ready for testing. The messierObjectsList is very fragile and with different LST values will crash. The parsing should use the letters for  h, m, and s to determie where to extract from the LST string.

--- cgemObserveMessierObjects.py
+++ cgemObserveMessierObjects.py
@@ -1,5 +1,8 @@
 # This program is setup to observe Messier objetcs.
 
+# I'm not using the alt/azi to determine if the object is above the horizon
+# and I should be.
+
 import convertRaDecToCgemUnits
 import cgemInterface
 import messierObjectList
@@ -9,25 +12,51 @@ if __name__ == '__main__':
 #    cgemInterface = CgemInterface(False)
     
     cgem = cgemInterface.CgemInterface(False)
-    ra   = convertRaDecToCgemUnits.Ra()
-    dec  = convertRaDecToCgemUnits.Dec()
+#    ra   = convertRaDecToCgemUnits.Ra()
+#   dec  = convertRaDecToCgemUnits.Dec()
     
-    print 'Enter a negative number for the RA hours wnd the loop will exit.'
-
     messierList = messierObjectList.MessierObjectList()
     
     print messierList.objectTable[0].name
     
     loopControl = True
+    index = 0
     while loopControl:
-        timeToQuit = input ("Enter a negative 1 to quit ")
-        if timeToQuit == -1:
-            loopControl = False
-        else:
-            messierList.objectTable[0].write()
+#        timeToQuit = input ("Enter a negative 1 to quit ")
+#        if timeToQuit == -1:
+#            loopControl = False
+#        else:
+#        print 'binNumber: ', messierList.objectTable[index].bin()
+#        print 'index    : ', index
+        if messierList.objectTable[index].bin() > 0:
+            messierList.objectTable[index].write()
+            
+            x = input('1 to observe, 2 to skip, 3 to exit ')
+            if x == 1:
+                ra = messierList.objectTable[index].ra
+                dec = messierList.objectTable[index].dec
+            
+                newRa = convertRaDecToCgemUnits.Ra()
+                newRa.hr = float(ra.hr)
+                newRa.min = float(ra.min)
+                newRa.sec = float(ra.sec)
+            
+                newDec = convertRaDecToCgemUnits.Dec()
+                newDec.deg = float(dec.deg)
+                newDec.min = float(dec.min)
+                newDec.sec = float(dec.sec)
+    
+                cgem.gotoCommandWithHP (newRa, newDec)
+            else:
+                if x == 3 or (index == len(messierList.objectTable)-1):
+                    loopControl = False
+                    
+            # print dec.deg, dec_min, deg_sec
 #            dec = messierList.objectTable[0].dec
 #            cgem.gotoCommand (ra,dec)
 #            print messierObjectList.objectTable[0].name
+
+        index += 1
 
 #    cgemInterface.close()
     
