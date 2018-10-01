2c44d1ffdb0eb40ae3e1caef1d92f9a09fabd69a
Zach Steele <zach@steelewoolsoftware.com>
2018-10-01 08:54:52 -0700

    New telescope simulator class

--- simulator.py
+++ simulator.py
@@ -5,23 +5,27 @@ ser = serial.Serial(
     port = './pty2',
     timeout=1)
 
+class TelescopeSim:
+  
+  def parse_command(self, prefix):
+    if prefix == 'r':
+      commandText = ser.read(20)
+      print "Read command parameters: '{0}'".format(commandText)
+      args = commandText.split(',', 2)
+      ser.write("#")
+    else:
+      print "Unknown command"
 
-def parse_command(prefix):
-  if prefix == 'r':
-    commandText = ser.read(20)
-    print "Read command parameters: '{0}'".format(commandText)
-    ser.write("#")
-  else:
-    print "Unknown command"
-
-done = False
-
-while not done:
-  cmd = ser.read(1)
-  if cmd != '':
-    print "Read command-char '{0}'".format(cmd)
-    parse_command(cmd)
-  if cmd == 'q':
-    print "Ready to quit"
-    done = True
+if __name__ == '__main__':
+  done = False
+  sim = TelescopeSim()
+  
+  while not done:
+    cmd = ser.read(1)
+    if cmd != '':
+      print "Read command-char '{0}'".format(cmd)
+      sim.parse_command(cmd)
+    if cmd == 'q':
+      print "Ready to quit"
+      done = True
 
