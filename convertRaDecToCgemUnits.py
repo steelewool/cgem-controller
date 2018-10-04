cce5d849da1aff162c3090208a993a8488658326
Rob Steele <robert@steelewoolsoftware.com>
2018-10-04 09:40:56 -0700

    In the middle of bug fixes.

--- convertRaDecToCgemUnits.py
+++ convertRaDecToCgemUnits.py
@@ -1,5 +1,6 @@
 import serial
-    
+import numpy
+
 # Algorith doesn't handle negavite degrees. Negative angle much be translated
 # to be between 270 and 360 degrees.
 
@@ -17,43 +18,77 @@ class CgemConverter:
     # This only works when instantiating child classes (Ra and Dec)
     def __init__(self):
         self.toCgem()
+        self.fromCgem(cgemUnits = ' ')
 
     # Compute the ra/hex value, store as hex but return as string
     def convertSeconds(self, seconds):
+        print 'seconds :', seconds
         self.gotoValue = seconds * 12.0 * CgemConverter.conversionFactor
+        print 'self.gotoValue: ', self.gotoValue
         self.hexGotoValue = hex(int(self.gotoValue))
         self.strGotoValue = hex(int(self.gotoValue))[2:]
         x = len(self.strGotoValue)
 #        print 'x            : ', x
         addCharacters = 8-x
-#        print 'strGotoValue : ', self.strGotoValue
-#        print 'addCharacters: ', addCharacters
+        print 'strGotoValue : ', self.strGotoValue
+        print 'addCharacters: ', addCharacters
         for i in range (0,addCharacters):
             self.strGotoValue += '0'
+        print 'self.strGotoValue: ', self.strGotoValue
         return self.strGotoValue
 
 class Ra(CgemConverter):
     hr  = 0.0
     min = 0.0
     sec = 0.0
+    raCgemUnits = '0'
     
     def toCgem(self):
         self.raInSeconds     = (self.hr * 60.0 * 60.0 + self.min  * 60.0 + self.sec) * 15.0
+        print 'self.raInSeconds: ', self.raInSeconds
         return str.upper(self.convertSeconds(self.raInSeconds))
-
+    
+    def fromCgem(self, cgemUnits):
+        #x = hex(int(cgemUnits, 16)) >> 8
+        #print x
+        #print hex(x)
+        # lowByte = int(cgemUnits, 16)
+        # print 'lowByte: ', lowByte
+        return cgemUnits
+    
 class Dec(CgemConverter):
     deg = 0.0
     min = 0.0
     sec = 0.0
+    decCgemUnits = '0'
     
     def toCgem(self):
         self.decInSeconds    =  abs(self.deg) * 60.0 * 60.0 + self.min * 60.0 + self.sec
-        
+#        print 'self.decInSeconds: ', self.decInSeconds
         if (self.deg < 0):
             self.decInSeconds = (360.0 * 60.0 * 60.0) - self.decInSeconds;
         
-        return str.upper(self.convertSeconds(self.decInSeconds))
+        gotoValue = self.decInSeconds * 12.0 * CgemConverter.conversionFactor
+        
+#        print 'gotoValue : ', gotoValue
+        
+        hexGotoValue = hex(int(gotoValue) << 8)
+        strGotoValue = str(hexGotoValue)[2:]
+        addCharacters = 8-len(strGotoValue)
+        for i in range (0,addCharacters):
+            strGotoValue += '0'
 
+        return str.upper(strGotoValue)
+    
+        # return str.upper(self.convertSeconds(self.decInSeconds))
+
+    def fromCgem (self, cgemUnits):
+        # y = numpy.uint32(x)
+        # x = numpy.uint64 (cgemUnits)
+        # lowByte = int(cgemUnits) & 0xFF
+        # print x
+        # lowByte = x  & 255
+        return cgemUnits
 # This paradigm was provided by Zach as a way to test the individual
 # classes as a main program.
 
@@ -78,23 +113,22 @@ if __name__ == '__main__':
     print 'oneTwelthArcSeconds  : ', CgemConverter.oneTwelthArcSeconds
     print 'conversionFactor     : ', CgemConverter.conversionFactor
     
-# This fails if there is no serial device. Need someway to test without the
-# hardware being present. Until then I'll put a #@ in front of serial
-# commands until I get this resolved
-    
-#@    ser = serial.Serial('/dev/ttyUSB0', timeout=1)
-#@    print 'ser name : ', ser.name
     print 'write to the serial: ', 'r' + ra.toCgem() + ',' + dec.toCgem()
-    
-#@    ser.write ('r' + conversion.strRaGotoValue + ',' + conversion.strDecGotoValue)
-#@    char = ser.read(100)
-#@    print 'char     : ', char
     
     raCgemUnits  = ra.toCgem()
     decCgemUnits = dec.toCgem()
     
     print 'raCgemUnits  : ', raCgemUnits
     print 'decCgemUnits : ', decCgemUnits
-    
-#@    ser.close()
 
+    print 'RA  fromCgem : ', ra.fromCgem(raCgemUnits)
+    print 'Dec fromCgem : ', dec.fromCgem(decCgemUnits)
+    
+    # worked: print str.upper(str(hex((int(decCgemUnits, 16) >> 8) & 0xff))[2:])
+    x = int(decCgemUnits,16) >> 8
+    print ' x and hex(x) ', x, hex(x)
+    print 'conversionFactor: ', CgemConverter.conversionFactor
+    seconds = int(x / 12.0 / CgemConverter.conversionFactor)
+    print seconds
+    
+    
