63e7b57e2c39d2b6a633672debc2e4e12615e000
Rob Steele <robert@steelewoolsoftware.com>
2018-11-16 22:41:43 -0800

    Added logic to check that the files created by nullmodem.sh and debugTty.sh

--- spawnSimulator.py
+++ spawnSimulator.py
@@ -1,8 +1,11 @@
-# Provide basic goto operations for a manually entered RA/Declination
 
 import os
 import signal
 import time
+
+# Kicks off either the nullmodem.sh script or debugTty.sh
+# The debugTty.sh script is used for debugging hardware.
+# The nullModem.sh and simulatory.py are using when running the simulator.
 
 class SpawnSimulator:
 
@@ -14,21 +17,40 @@ class SpawnSimulator:
         if self.simulate == True:
             self.pid_modem = os.spawnlp(os.P_NOWAIT,
                                         "./nullmodem.sh", "", "")
-            time.sleep(1)
+
+            # Check that both pty1 and pty2 files exist before continuing
+            
+            checkFileExistance = True
+            while checkFileExistance:
+                checkFileExistance1 = os.path.exists("pty1")
+                checkFileExistance2 = os.path.exists("pty2")
+                checkFileExistance  = not(checkFileExistance1 and
+                                          checkFileExistance2)
+                
             self.pid_python = os.spawnlp(os.P_NOWAIT,
                                          "python", "", "simulator.py")
         else:
             self.pid_modem = os.spawnlp(os.P_NOWAIT,
                                             "./debugTty.sh", "", "")
 
+            # Check that both pty1 and /dev/ttyUSB0 exists before continuing
+            
+            checkFileExistance = True
+            while checkFileExistance:
+                checkFileExistance1 = os.path.exists("pty1")
+                checkFileExistance2 = os.path.exists("/dev/ttyUSB0")
+                checkFileExistance  = not(checkFileExistance1 and
+                                          checkFileExistance2)
+
     def shutdown(self):
         if self.simulate == True:
-            time.sleep(1)
+#            time.sleep(1)
             os.kill(self.pid_python, signal.SIGSTOP)
         os.kill(self.pid_modem, signal.SIGSTOP);
 
 if __name__ == '__main__':
     sp = SpawnSimulator(True)
+    print "SpawnSimulator, sleep 5"
     time.sleep(5)
     sp.shutdown()
 
